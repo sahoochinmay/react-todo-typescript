85724dfda5b014f1236cbae3cb460d50595eec2c
sahoochinmay <sahoochinmay54@gmail.com>
2023-07-22 20:52:41 +0530

    initial commit

---
+++ src/components/Todos.tsx
@@ -0,0 +1,35 @@
+import { useTodos } from "../store/todos";
+import { useSearchParams } from "react-router-dom";
+
+const Todos = () => {
+  const { todos, toggleTodoAsCompleted, handleDeleteTodo } = useTodos();
+  const [searchParams] = useSearchParams();
+  const todosData = searchParams.get("todos");
+
+  let filterData = todos;
+  if (todosData === "active") {
+    filterData = filterData.filter((task) => !task.completed);
+  }
+  if (todosData === "completed") {
+    filterData = filterData.filter((task) => task.completed);
+  }
+  return (
+    <ul className="main-task">
+      {filterData?.map((todo) => {
+        return (
+          <li key={todo.id}>
+            <input type="checkbox" name="" id={`todo-${todo.id}`} checked={todo.completed} onChange={() => toggleTodoAsCompleted(todo.id)} />
+            <label htmlFor={`todo-${todo.id}`}>{todo.task}</label>
+            {todo.completed && (
+              <button type="button" onClick={() => handleDeleteTodo(todo.id)}>
+                Delete
+              </button>
+            )}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Todos;
